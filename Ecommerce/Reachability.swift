146489695416c81279c53925f35468c76e9996a6
neethusadasivan <neethusadasivan8@gmail.com>
2017-11-13 13:07:13 +0530

    Added network class

--- Ecommerce/Reachability.swift
+++ Ecommerce/Reachability.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2017 Neethu Sadasivan. All rights reserved.
 //
 
+
 import Foundation
+import SystemConfiguration
+
+public class Reachability {
+    
+    class func isConnectedToNetwork() -> Bool {
+        
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        return (isReachable && !needsConnection)
+    }
+    
+}
